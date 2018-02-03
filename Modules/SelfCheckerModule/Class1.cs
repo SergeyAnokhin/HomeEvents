2b3d84555977c7df85d0cd3b4350ab075af60b7e
Sergey ANOKHIN <star0s@mail.ru>
2018-02-03 17:44:19 +0100

    create solution structure

---
+++ Modules/SelfCheckerModule/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfCheckerModule
+{
+    public class Class1
+    {
+    }
+}
